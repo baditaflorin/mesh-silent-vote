4b4899cf94bbf210cf0d65f04c22a5a0d815ba07
Florin Badita <baditaflorin@gmail.com>
2026-05-19 00:04:59 +0300

    feat: mount MeshBeacon next to InviteShareButton in non-MeshShell holdouts

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { VoteRoom } from "./features/vote/VoteRoom";
 import { SettingsDrawer } from "./features/settings/SettingsDrawer";
 import { appConfig } from "./shared/config";
-import { InviteShareButton } from "@baditaflorin/mesh-common";
+import { InviteShareButton, MeshBeacon } from "@baditaflorin/mesh-common";
 
 const STORAGE = {
   room: `${appConfig.storagePrefix}:room`,
@@ -35,6 +35,8 @@ export function App() {
       <VoteRoom roomId={roomId} peerId={peerId} />
 
       <InviteShareButton appName={appConfig.appName} roomId={roomId} />
+      <MeshBeacon app={appConfig.appName} room={roomId} />
+
       <button
         type="button"
         className="settings-fab"
